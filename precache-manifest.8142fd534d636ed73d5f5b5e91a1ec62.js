899ec0b49d678f684a091a2e717a58155303f20a
Inácio Schweller <iscmendonca@gmail.com>
2019-03-16 21:26:50 -0300

    Updates

---
+++ precache-manifest.8142fd534d636ed73d5f5b5e91a1ec62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4f7a8a0cfd2bf3433909",
+    "url": "/static/js/main.dd634140.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "4f7a8a0cfd2bf3433909",
+    "url": "/static/css/main.9a11efac.chunk.css"
+  },
+  {
+    "revision": "5252cfeb20fa460735bea6afd5f30e47",
+    "url": "/index.html"
+  }
+];
