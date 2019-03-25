d3baffae58ced2f0fa5ce21aa0240c1915498e00
Inácio Schweller <iscmendonca@gmail.com>
2019-03-25 03:11:40 -0300

    Added Entry component and work experience section

--- src/screens/App/components/Background.js
+++ src/screens/App/components/Background.js
@@ -2,6 +2,25 @@ import React from 'react';
 import * as THREE from 'three'
 import { vertexShader, fragmentShader } from '../shaders'
 
+const generateParticlesPositionScale = (gap, x, y) => {
+  const totalParticles = x * y
+  let positions = new Float32Array( totalParticles * 3 )
+  let scales = new Float32Array( totalParticles )
+  let i = 0, j = 0;
+  for ( let ix = 0; ix < x; ix++ ) {
+    for ( let iy = 0; iy < y; iy++ ) {
+      positions[ i ] = ix * gap - ( ( x * gap ) / 2 ) // x
+      positions[ i + 1 ] = 0 // y
+      positions[ i + 2 ] = iy * gap - ( ( y * gap ) / 2 ) // z
+      scales[ j ] = 1
+      i += 3
+      j ++
+    }
+  }
+
+  return { positions, scales }
+} 
+
 const Particles = () => {
   let count=0
   const { useRef, useEffect, useState } = React
@@ -13,29 +32,15 @@ const Particles = () => {
     let width = mount.current.clientWidth
     let height = mount.current.clientHeight
     let mouseX = 1
-    let mouseY = 0 
     let frameId
-    const SEPARATION = 100, AMOUNTX = 300, AMOUNTY = 200
-    
+
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(75, width / height, 1, 10000)
     camera.position.y = 1000
     camera.position.z = 100
     
-    const numParticles = AMOUNTX * AMOUNTY
-    var positions = new Float32Array( numParticles * 3 )
-		var scales = new Float32Array( numParticles )
-    var i = 0, j = 0;
-    for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
-      for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-        positions[ i ] = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 ); // x
-        positions[ i + 1 ] = 0; // y
-        positions[ i + 2 ] = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 ); // z
-        scales[ j ] = 1;
-        i += 3;
-        j ++;
-      }
-    }
+    const SEPARATION = 100, AMOUNTX = 300, AMOUNTY = 200
+    const { positions, scales } = generateParticlesPositionScale(SEPARATION, AMOUNTX, AMOUNTY)
     
     const geometry = new THREE.BufferGeometry();
     geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ))
@@ -73,9 +78,9 @@ const Particles = () => {
       for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
         for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
           positions[ i + 1 ] = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) +
-            ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
+            ( Math.sin( ( iy + count ) * 0.5 ) * 50 )
           scales[ j ] = ( Math.sin( ( ix + count ) * 0.1 ) + 1 ) * 3 +
-            ( Math.sin( ( iy + count ) * 0.1 ) + 1 ) * 3;
+            ( Math.sin( ( iy + count ) * 0.1 ) + 1 ) * 3
           i += 3;
           j ++;
         }
@@ -157,7 +162,7 @@ const Particles = () => {
     }
   }, [isAnimating])
   
-  return <div className="particles" ref={mount} onClick={() => setAnimating(!isAnimating)} />
+  return <div className="Particles" ref={mount} onClick={() => setAnimating(!isAnimating)} />
 }
 
 export default Particles
--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -1,7 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useSpring, animated } from 'react-spring'
 import Particles from './components/Background'
+import Entry from './components/Entry'
 import './App.css';
+
+const entries = [
+  {
+    position: "Senior Software Engineer",
+    company: "Fanatics",
+    from: "September 2017 - Current"
+  },
+  {
+    position: "Software Engineer",
+    company: "Avenue Code",
+    from: "June 2015 - July 2017"
+  },
+  {
+    position: "Frontend Developer",
+    company: "PSafe Tecnologia",
+    from: "September 2014 - June 2015"
+  },
+  {
+    position: "Frontend Developer",
+    company: "Infoglobo",
+    from: "February 2011 - September 2014"
+  }  
+]
 
 function App () {
   const [loading, setLoading] = useState(true)
@@ -12,24 +36,49 @@ function App () {
 
   //TODO show a loader
   useEffect(() => {
-    setTimeout(setLoading(false), 1000)
+    let timer = setTimeout(() => setLoading(false), 800)
+    return () => clearTimeout(timer)
   }, [])
 
   return loading ?
       <div className="loader">Loading</div> :
       <animated.div className="App" style={{opacity: opacity.interpolate(o => 0 + o) }}>
         <Particles />
-        <div className="App-hero">
-          <div className="cutive-mono align-left">
-            <p>Hi, I'm Inacio!</p>
-            <p>I work and freelance as software engineer and game developer.</p>
-            <p>Recently I placed 4th on Alakajam, a game jam, with "Cast Die Repeat".</p>
-          </div>       
+        <div className="Hero">
+          <div className="Row">
+            <div className="Hero-intro Column">
+              <div className="roboto-mono light align-left">
+                <p>Hello! I'm Inacio. I work and freelance as software engineer</p>
+                <p>I've proudly collaborated with awesome companies</p>
+                <p>4th on Alakajam, a game jam, with <a 
+                    className="underline-wave" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    href="https://thesunda.itch.io/cast-die-repeat">
+                    Cast Die Repeat
+                  </a>
+                </p>
+              </div>       
+            </div>
+            <div className="Hero-contact Column">
+              <p>Get in touch</p>
+              <ul>
+                <li>iscmendonca@gmail.com</li>
+                <li></li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className="App-hero">      
+        <div className="Scroll">
+          <h3 className="roboto-mono light">work experience></h3>
+          {entries.map((entry, key) => {
+            return <Entry key={key} entry={entry}/>
+          })}
+          <div className="Footer">
+            <p className="roboto-mono light">Made by yours truly. © 2019</p>
+          </div>
         </div>
       </animated.div>
-     
 }
 
 export default App;
