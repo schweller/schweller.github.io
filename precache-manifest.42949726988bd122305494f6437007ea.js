729fcf5980c9e234ec959e07653e10ea088be6e7
Inácio Schweller <iscmendonca@gmail.com>
2019-11-28 00:30:17 -0300

    Updates

---
+++ precache-manifest.42949726988bd122305494f6437007ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc30e115c5a5936865e1db489c44a5c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c925120d3ab963114fcd",
+    "url": "/static/css/main.58658601.chunk.css"
+  },
+  {
+    "revision": "8e5e7ef0949773571dbe",
+    "url": "/static/js/2.267cdf64.chunk.js"
+  },
+  {
+    "revision": "c925120d3ab963114fcd",
+    "url": "/static/js/main.2adeb03b.chunk.js"
+  },
+  {
+    "revision": "2e74d11fb1214e730b05",
+    "url": "/static/js/runtime-main.23c7c8a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a42594ed1d18b4a2e6a823b350c7912.js"
+  "/precache-manifest.42949726988bd122305494f6437007ea.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
